eeb029565bbf5141984f44a571791949d1b2e6f3
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-05-22 13:08:34 +0530

    recursion next ques

--- src/recursions.java
+++ src/recursions.java
@@ -9,9 +9,23 @@ public class recursions {
         print1toN();
 
     }
+
+    static void printName(String name,int N ){
+        if (cnt>N) return;
+        System.out.println(name);
+        cnt++;
+        printName(name,N);
+    }
+    static void printNto1( int N){
+        if(N==0) return;
+        System.out.println(N);
+        printNto1(N-1);
+    }
     public static void main(String[] args) {
 
-        print1toN();
+        //print1toN();
+        //printName("Akshita",5);
+        printNto1(10);
 
     }
 }
