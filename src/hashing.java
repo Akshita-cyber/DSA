35d695820a657b5fec23bede198fa2099f981c33
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-09 14:11:09 +0530

    number hashing

---
+++ src/hashing.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class hashing {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Input size of arrays");
+        Scanner sc=new Scanner(System.in);
+        n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Input elements of array");
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int [] hash =new int[13];// hash array
+        for (int i=0;i<n;i++) {
+            hash[arr[i]]+=1;
+        }
+
+        int q;// no of times you want to check
+        System.out.println("enter no of times you want to check");
+        q=sc.nextInt();
+        for(int i = 0; i<q; i++){
+            int number;
+            number=sc.nextInt();
+            System.out.println(hash[number]);
+        }
+
+    }
+}
