bdc304d1d4f5ff3bf85cb93542ab484c5f43c015
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-09 18:20:01 +0530

    char hashing if strings contains both lower & upper case

--- src/characterHashing.java
+++ src/characterHashing.java
@@ -7,11 +7,27 @@ public class characterHashing {
 
         //1. for only small case or only large case stings
         //precompute
-        int [] hash = new int[26];
-        for ( int i=0;i<s.length();i++){
-            hash[s.charAt(i)-'a']+=1;
-        }
+//        int [] hash = new int[26];
+//        for ( int i=0;i<s.length();i++){
+//            hash[s.charAt(i)-'a']+=1;
+//        }
+//
+//        int q;
+//        System.out.println("how many characters you want to check");
+//        q =sc.nextInt();
+//        while(q-->0){
+//            System.out.println("enter character you want to search");
+//            char c;
+//            c=sc.next().charAt(0);
+        //fetch
+//            System.out.println(hash[c-'a']);
+//        }
 
+        //2. Code if the string contains both uppercase and lowercase:
+        int []hash=new int[256];
+        for (int i =0;i<s.length();i++){
+            hash[s.charAt(i)]+=1;
+        }
         int q;
         System.out.println("how many characters you want to check");
         q =sc.nextInt();
@@ -19,8 +35,8 @@ public class characterHashing {
             System.out.println("enter character you want to search");
             char c;
             c=sc.next().charAt(0);
-            System.out.println(hash[c-'a']);
+            //fetch
+            System.out.println(hash[c]);
         }
-
     }
 }
