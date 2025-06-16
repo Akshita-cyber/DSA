8bef3a1c35575a78f0f2b0a31acc73b0fc2a3f8d
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-17 00:48:48 +0530

    some updates checking

--- src/Arrays/easyL/IsArraySorted.java
+++ src/Arrays/easyL/IsArraySorted.java
@@ -1,4 +1,20 @@
 package Arrays.easyL;
 
 public class IsArraySorted {
+    static boolean isSorted(int [] arr, int n){
+
+        for (int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int [] arr={23, 50,12, 99, 10, 5};
+        System.out.println(isSorted(arr,6));
+
+        int [] arr2={1,2,3,4,5,6};
+        System.out.println(isSorted(arr2,6));
+    }
 }
