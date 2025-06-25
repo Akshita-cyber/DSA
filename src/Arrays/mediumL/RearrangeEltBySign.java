10fe8a5556e51649c384ddef43c9997be70fc705
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-25 16:58:19 +0530

    variety 2 of same question

--- src/Arrays/mediumL/RearrangeEltBySign.java
+++ src/Arrays/mediumL/RearrangeEltBySign.java
@@ -1,5 +1,5 @@
 package Arrays.mediumL;
-
+import java.util.*;
 public class RearrangeEltBySign {
 //    Input:
 //    arr[] = {1,2,-4,-5}, N = 4
@@ -21,12 +21,79 @@ public class RearrangeEltBySign {
 
     }
 
-    public static void main(String[] args) {
-        int arr[] = {1,2,-4,-5};
-        int ans[]=RearrangebySign(arr,4);
-        for(int val: ans){
+    static int [] RearrangebySign2(int []arr,int n){
+        int ans[]=new int[n];
+        int j=-1;
+        int posIndex=0,negIndex=1;
+        for(int i=0;i<n;i++) {
+            if(posIndex <n || negIndex<n) {
+                if (arr[i] >= 0 && posIndex <n ) {
+                    ans[posIndex] = arr[i];
+                    posIndex += 2;
+                    j++;
+                } else if (negIndex<n){
+                    ans[negIndex] = arr[i];
+                    negIndex += 2;
+                    j++;
+                }
+            }
+            else{
+                j++;
+                ans[j]=arr[i];
+
+            }
+        }
+        System.out.println(ans.length);
+        return ans;
+
+    }
+    static void RearrangebySign3(int [] arr,int n){
+        ArrayList<Integer> pos=new ArrayList<>();
+        ArrayList<Integer> neg=new ArrayList<>();
+
+        for (int i=0;i<n;i++){
+            if (arr[i]>0) pos.add(arr[i]);
+            if (arr[i]<0) neg.add(arr[i]);
+        }
+
+        int [] ans=new int[n];
+        int p=0,ne=0;
+        int i=0;
+        while (p<pos.size() && ne<neg.size()){
+            ans[i++]=pos.get(p++);
+            ans[i++]=neg.get(ne++);
+        }
+
+       //add remaining elements
+        while(p<pos.size()){
+            ans[i++]=pos.get(p++);
+        }
+        while(n<neg.size()){
+            ans[i++]=neg.get(ne++);
+        }
+        for(int val:ans){
             System.out.print(val+" ");
         }
+
+    }
+
+    public static void main(String[] args) {
+        //int arr[] = {1,2,-4,-5};
+        //int ans[]=RearrangebySign(arr,4);
+        int arr[] = {1,2,-4,-5,3,4};
+
+        //variety 2
+//        Input:
+//        arr[] = {1,2,-4,-5,3,4}, N = 6
+//        Output:
+//        1 -4 2 -5 3 4
+
+        //int ans[]=RearrangebySign2(arr,6);//<-but this is not a good approach for variety 2
+        //for(int val: ans){
+        //    System.out.print(val+" ");
+        //}
+
+        RearrangebySign3(arr,6);
     }
 
 }
